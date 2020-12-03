e0e3cb1be0e2e82967aa8b8238071df34f935036
Samara Grant <samara.amanda01@gmail.com>
2020-12-03 21:37:31 +0000

    Automatically backed up by Learn

--- db/migrate/20201203213700_03_add_column_to_owners.rb
+++ db/migrate/20201203213700_03_add_column_to_owners.rb
@@ -1,4 +1,9 @@
 class 03AddColumnToOwners < ActiveRecord::Migration[5.2]
-  def change
-  end
+  def up
+      add_column :owners, :pet_id, :integer
+    end
+
+    def down
+      remove_column :owners, :pet_id
+    end
 end
